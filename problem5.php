1a636559b0f195cf50effd9b624f958b6de4f918
Abdullah Al Nahian <info.nahian13@gmail.com>
2024-06-11 17:11:07 +0600

    Sum numbers horizontally

--- problem5.php
+++ problem5.php
@@ -1,23 +1,43 @@
 <?php 
 
 /**
- * 
+ *  Problem solving
+ *  Q. Sum numbers horizontally
+ *  A. $numbers = 62343; // 18
  */
-// $numbers = 62343;
-$numbers = 2000;
-function sumNumbers($numbers)
+$numbers = 62343;
+function sumNumbersSolutionOne($numbers)
 {
     $result = 0;
     $lastDigit = 0;
     for($i=0;$i<strlen($numbers); $i++){
-        $lastDigit = $numbers % 10; 
-        if($lastDigit > 0){
-            $result = $result + $lastDigit;
-            $numbers = $numbers / 10;
-        }
+        $lastDigit = $numbers % 10;
+       if($lastDigit > 0){
+        $result = $result + $lastDigit;
+        $numbers = $numbers / 10;
+       }
     }
     return $result;
 }
 
- echo sumNumbers($numbers);
- 
+ sumNumbersSolutionOne($numbers);
+
+
+/**
+ *  Problem solving
+ *  Q. Sum numbers horizontally
+ *  A. $numbers = 1000; // 1
+ */
+$numbers = 1000;
+function sumNumbersSolutionTwo($numbers)
+{
+    $result = 0;
+    while($numbers > 0){
+        $lastDigit = $numbers % 10;
+        $result = $result + $lastDigit;
+        $numbers = intdiv($numbers, 10);
+    }
+    return $result;
+}
+
+echo sumNumbersSolutionTwo($numbers);
